338a3ce0ed5548f30e44e1497b8802820a509a60
David Castellar Martinez <castellardav@gmail.com>
2022-10-15 23:22:13 -0500

    termina primer ejercicio

---
+++ src/entrega01/ejercicio01/ejercicio01.java
@@ -0,0 +1,54 @@
+package entrega01.ejercicio01;
+
+import java.util.Scanner;
+
+public class ejercicio01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int numero;
+
+        System.out.print("Ingresa un número en el rango [0-15]: ");
+        numero = input.nextInt();
+
+        System.out.println(convertirNumeroAPalabra(numero));
+    }
+
+    public static String convertirNumeroAPalabra(int numero) {
+        switch (numero) {
+            case 0:
+                return "cero";
+            case 1:
+                return "uno";
+            case 2:
+                return "dos";
+            case 3:
+                return "tres";
+            case 4:
+                return "cuatro";
+            case 5:
+                return "cinco";
+            case 6:
+                return "seis";
+            case 7:
+                return "siete";
+            case 8:
+                return "ocho";
+            case 9:
+                return "nueve";
+            case 10:
+                return "diez";
+            case 11:
+                return "once";
+            case 12:
+                return "doce";
+            case 13:
+                return "trece";
+            case 14:
+                return "catorce";
+            case 15:
+                return "quince";
+            default:
+                return "unknown";
+        }
+    }
+}
---
+++ src/entrega01/ejercicio01/readme.md
@@ -0,0 +1,9 @@
+### ENUNCIADO
+
+Crear un programa que convierta un número en letras (por ejemplo: 15 en “quince”).
+Criterios de aceptación:
+
+● El número puede estar en un rango de 0 a 15.\
+● Si el número no corresponde al rango [0-15] asignarle
+valor 'unknown'.\
+● Imprimir el resultado por el terminal.
