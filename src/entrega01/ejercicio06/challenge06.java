4b8258bddfd8c7710060a64345968230a9e85edf
David Castellar Martinez <castellardav@gmail.com>
2022-10-16 00:23:40 -0500

    termina challenge 6

---
+++ src/entrega01/ejercicio06/challenge06.java
@@ -0,0 +1,34 @@
+package entrega01.ejercicio06;
+
+import java.util.Locale;
+
+public class challenge06 {
+    public static void main(String[] args) {
+        String oracion = "Esta forma estoy entendiendo.";
+        String oraciónMinusculas = oracion.toLowerCase();
+        char caracterMasRepetitivo = Character.MIN_VALUE;
+        int conteoCaracterMasRepetitivo = 0;
+
+        for (int i = 0; i < oraciónMinusculas.length(); i++) {
+            char caracterActual = oraciónMinusculas.charAt(i);
+            int conteoCaracter = 1;
+
+            if (caracterActual != caracterMasRepetitivo && caracterActual != ' '
+                    && caracterActual != ',' && caracterActual != '.') {
+                for (int j = i + 1; j < oraciónMinusculas.length(); j++) {
+                    if (oraciónMinusculas.charAt(j) == caracterActual) {
+                        conteoCaracter++;
+                    }
+                }
+            }
+
+            if (conteoCaracter > conteoCaracterMasRepetitivo) {
+                conteoCaracterMasRepetitivo = conteoCaracter;
+                caracterMasRepetitivo = caracterActual;
+            }
+        }
+
+        System.out.println(oracion);
+        System.out.printf("Carácter '%c', %d repeticiones%n", caracterMasRepetitivo, conteoCaracterMasRepetitivo);
+    }
+}
---
+++ src/entrega01/ejercicio06/readme.md
@@ -0,0 +1,17 @@
+### ENUNCIADO
+
+Cree un programa que evalúe una oración definida en el mismo código, y
+muestre cual es el carácter que más se repite y cuantas veces lo hace.
+
+Criterios de aceptación:
+
+● Debe contabilizar como un mismo carácter tanto las letras mayúsculas
+como minúsculas, sin diferenciarlas.\
+Ej: “Esta forma estoy entendiendo.” -> Caracter ‘e’, 5 repeticiones
+
+Nota: No debe incluir como carácter a evaluar, el espacio en blanco, ni puntos
+ni comas.
+
+TIP: Se puede resolver haciendo un for que tome un caracter y luego otro for que
+vuelva a recorrer en busca de coincidencias. Apoyarse en variables auxiliares
+para guardar caracter repetido y cantidad de repeticiones en cada iteración
